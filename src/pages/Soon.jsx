77fe24b21427f5feb69734bca20910ec77df9bbc
Kyriacoloco <kyriacos_93@hotmail.com>
2026-09-10 11:58:14 +0300

    Delete src directory

--- src/pages/Soon.jsx
+++
@@ -1,9 +0,0 @@
-import { useLocation } from "react-router-dom";
-export default function Soon() {
-  const name = useLocation().pathname.split("/").pop().replace("-", " ");
-  return (
-    <>
-      <div className="pagehead"><div><h1 style={{ textTransform: "capitalize" }}>{name}</h1><p>Comes in a later slice. The data it needs is already in the database.</p></div></div>
-    </>
-  );
-}
